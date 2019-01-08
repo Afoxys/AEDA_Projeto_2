9469b1664d3ed05f5b4af0d186dc79cf38e14270
Afoxys <38985979+Afoxys@users.noreply.github.com>
2019-01-08 21:36:33 +0000

    Visual Studio

---
+++ AEDAPROJETO2/AEDAPROJETO2/Company.cpp
@@ -0,0 +1,41 @@
+#include "pch.h"
+#include "Company.h"
+
+Company::Company() :clients(Client("",0,0)) {}
+
+
+Company::Company(BST<Client> clientTree) : clients(clientTree){
+}
+
+
+Company::~Company()
+{
+}
+
+bool Company::addClient(Client &clnt1) {
+	Client notFound("", 0, 0);
+	Client newClient(clnt1.getName(), clnt1.getNIF(), clnt1.getContact());
+	Client findAttempt = clients.find(newClient);
+
+	if (findAttempt == notFound) {
+		clients.insert(newClient);
+		return true;
+	}
+	else return false;
+}
+
+bool Company::removeClient(Client &clnt1) {
+	Client notFound("", 0, 0);
+	Client newClient(clnt1.getName(), clnt1.getNIF(), clnt1.getContact());
+	Client findAttempt = clients.find(newClient);
+
+	if (!(findAttempt == notFound)) {
+		clients.remove(newClient);
+		return true;
+	}
+	else return false;
+}
+
+BST<Client> Company::getClients() {
+	return this->clients;
+}
