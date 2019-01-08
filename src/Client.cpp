04c05b6da531552e411d795b12f80383df52c0dd
Afoxys <38985979+Afoxys@users.noreply.github.com>
2019-01-08 23:51:51 +0000

    Commentary for Doxygen

--- src/Client.cpp
+++ src/Client.cpp
@@ -1,7 +1,9 @@
 #include "pch.h"
 #include "Client.h"
 
-
+/**
+* @brief Default constructor for the class Client
+*/
 Client::Client(){
 	Client::name = "";
 	Client::nif = 0;
@@ -9,6 +11,12 @@ Client::Client(){
 	Client::clientProjects = {};
 }
 
+/**
+* @brief Constructor for the class Client
+* @param name - Name of the client
+* @param nif - NIF of the client
+* @param contact - Contact of the client
+*/
 Client::Client(string name, int nif, int contact) {
 	Client::name = name;
 	Client::nif = nif;
@@ -16,22 +24,38 @@ Client::Client(string name, int nif, int contact) {
 	Client::clientProjects = {};
 }
 
+/**
+* @brief Function to return the client's NIF
+*/
+
 int Client::getNIF() {
 	return this->nif;
 }
 
+/**
+* @brief Function to return the client's contact
+*/
 int Client::getContact() {
 	return this->contact;
 }
 
+/**
+* @brief Function to return the client's name
+*/
 string Client::getName() {
 	return this->name;
 }
 
+/**
+* @brief Function to return the client's projects
+*/
 int Client::getNumberProjects() {
 	return this->clientProjects.size();
 }
 
+/**
+* @brief Function to add an object of the class Project to the vector of projects
+*/
 void Client::addProject(Project proj) {
 	this->clientProjects.push_back(proj);
 }
@@ -39,6 +63,11 @@ void Client::addProject(Project proj) {
 
 Client::~Client(){}
 
+/**
+* @brief Operator function to return the comparison of two clients
+* @param clnt1 - object of the class Client
+* @return true if the client is bigger
+*/
 bool Client::operator<(const Client &clnt1) const {
 	if(this->clientProjects.size() == clnt1.clientProjects.size()) {
 		return(this->name < clnt1.name);
@@ -46,6 +75,11 @@ bool Client::operator<(const Client &clnt1) const {
 	else return(this->clientProjects.size() < clnt1.clientProjects.size());
 }
 
+/**
+* @brief Operator function to return the equality of two clients
+* @param clnt1 - object of the class Client
+* @return true if the clients are equal
+*/
 bool Client::operator==(const Client &clnt1) const {
 	return((this->name == clnt1.name) && (this->contact == clnt1.contact) && (this->nif == clnt1.nif));
 }
