ae0e735a6e4e1e4165690372cea0100b7181566b
BuildTools <unconfigured@null.spigotmc.org>
2019-01-09 00:34:40 +0000

    Aprendam a usar git sff

--- AEDAPROJETO2/AEDAPROJETO2/Project.h
+++ AEDAPROJETO2/AEDAPROJETO2/Project.h
@@ -1,14 +1,19 @@
 #pragma once
 
-/*This Class is left mostly undone because it's only being used as a shell to facilitate our project, since in the first part of the
+#include "Ticket.h"
+#include "BST.h"
+
+/*This Class is left undone because it's only being used as a shell to facilitate our project, since in the first part of the
 project we were not able to compile our code and thus we are not using the code from that part*/
 
 class Project
 {
-	int id;
 public:
-	Project(int id) { this->id = id; }
+	Project();
 	~Project();
-	int getId() const{ return id; }
+	priority_queue<Ticket> priorityQueue;
+	bool addTicket(Ticket ticket);
+	Ticket getMostPriority();
+	void returnTickets(vector<Ticket> tickets);
 };
 
